f7240d6ca885b939a28ad6824f96777a8d41804b
1213152524 <1213152524@qq.com>
2017-05-08 21:38:23 +0800

    1

--- js/L-dingdan2.js
+++ js/L-dingdan2.js
@@ -1,5 +1,6 @@
 $(function(){
-	$('.l-success .succ-x').on('touchstart',function(){
+	$('.l-success .succ-x').on('touchstart',function(e){
+		e.preventDefault();
 		$('.l-success').css('display','none')
 	})	
 })
